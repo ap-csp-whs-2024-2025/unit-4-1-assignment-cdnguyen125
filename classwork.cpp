6968db7e33df6cc760b4a3933fd027aa4bbd2809
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2024-12-11 21:03:47 +0000

    more outputs

--- classwork.cpp
+++ classwork.cpp
@@ -7,6 +7,9 @@ int main()
   double price = 19.99;
   bool isRaining = false; 
   
-  std::cout << age << " \n" << price << std::endl << isRaining; //to make space use " " // to make new line use "\n" or std::endl 
+  std::cout << age << " \n" << price << std::endl << isRaining << std::endl; //to make space use " " // to make new line use "\n" or std::endl 
+
+  int grade_level = 12;
+  std::cout << "I am a " << grade_level << "th grader" << std::endl;
   return 0;
 }
