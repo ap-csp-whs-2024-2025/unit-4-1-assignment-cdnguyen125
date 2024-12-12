1a9b20fb7a0abde038971c08ca69a6e07be046d6
cdnguyen125 <cdnguyen125@student.hbuhsd.edu>
2024-12-12 21:49:22 +0000

    what

--- classwork.cpp
+++ classwork.cpp
@@ -3,14 +3,50 @@
 
 int main()
 {
-  int age = 10;
-  double price = 19.99;
-  bool isRaining = false; 
-  
-  std::cout << age << " \n" << price << std::endl << isRaining << std::endl; //to make space use " " // to make new line use "\n" or std::endl 
+  // exercise 1
+ int x = 25;
+ int y = 3;
+ std::cout << "x + y = " << x+y << std::endl;
+ std::cout << "x - y = " << x-y << std::endl;
+ std::cout << "x * y = " << x*y << std::endl;
+ std::cout << "x / y = " << x/y << std::endl;
+ std::cout << "x % y = " << x%y << std::endl;
+ int z = 10;
+ std::cout << (x>z) << std::endl;
+ std::cout << (y<x) << std::endl << std::endl;
 
-  int grade_level = 12;
-  std::cout << "I am a " << grade_level << "th grader" << std::endl;
-  
+//exercise 2
+bool isSunny = true;
+bool hasUmbrella = false;
+std::cout << (isSunny && hasUmbrella) << std::endl;
+std::cout << (isSunny || hasUmbrella) << std::endl;
+std::cout << (!isSunny) << std::endl;
+std::cout << (!isSunny && !hasUmbrella) << std::endl << std::endl;
+
+//exercise 3
+int n = 7;
+double m = 4.0;
+bool hasLicense = false;
+std::cout << (n > m) << std::endl;
+std::cout << (m==4.0) << std::endl;
+std::cout << (hasLicense && m<=n) << std::endl;
+std::cout << (n>5 && !hasLicense) << std::endl << std::endl;
+
+//exercise 4
+std::string firstName = "John";
+std::string lastName = "Doe";
+double score = 85.75;
+std::cout << lastName << ", " << firstName << std::endl;
+std::cout << "Your score is: " << score << std::endl << std::endl;
+
+//exercise 6?
+int age = 18;
+double height = 5.9;
+bool isStudent = true;
+std::string city = "New York";
+std::cout << "You were born in the year " << 2024 - age << std::endl;
+std::cout << (age >= 18) << std::endl;
+std::cout << (height > 6) << std::endl;
+
   return 0;
 }
